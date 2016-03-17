3fd33705873d3f0285e824cf0f445835c1eb3d1a
truonghaui0910 <truonghaui0910@gmail.com>
2016-03-17 21:24:48 +0700

    upcode

--- components/com_home/layout.php
+++ components/com_home/layout.php
@@ -1,1 +1,249 @@
-HOME
+<div class="right_col" role="main">
+
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3> Slot 18 </h3>
+            </div>
+
+            <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>Hot Porn</h2>
+                        <!--                        <ul class="nav navbar-right panel_toolbox">
+                                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                                    </li>
+                                                    <li class="dropdown">
+                                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                                        <ul class="dropdown-menu" role="menu">
+                                                            <li><a href="#">Settings 1</a>
+                                                            </li>
+                                                            <li><a href="#">Settings 2</a>
+                                                            </li>
+                                                        </ul>
+                                                    </li>
+                                                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                                    </li>
+                                                </ul>-->
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+
+                        <div class="row">
+
+                            <!--<p>Media gallery design emelents</p>-->
+
+
+
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-55 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                            <!--                                            <div class="tools tools-bottom">
+                                                                                            <a href="#"><i class="fa fa-link"></i></a>
+                                                                                            <a href="#"><i class="fa fa-pencil"></i></a>
+                                                                                            <a href="#"><i class="fa fa-times"></i></a>
+                                                                                        </div>-->
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+                        </div>
+                    </div><!--end content noi dung-->
+                </div>
+            </div><!--end col-md-->
+
+            <div class="col-md-4">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>PORN HOT</h2>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <div class="row">
+                            <div class="col-md-3 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 animated flipInY">
+                                <div class="thumbnail">
+                                    <div class="image view view-first">
+                                        <a class="links"  href="http://google.com">
+                                            <img style="width: 100%; display: block;" src="images/4.jpg" alt="image" />
+                                        </a>
+                                        <div class="mask no-caption">
+                                        </div>
+                                    </div>
+                                    <div class="caption">
+                                        <p><strong>Asian girl masturbation</strong></p>
+                                        <p>Snow and Ice Incoming</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
--- templates/tem_new/home.php
+++ templates/tem_new/home.php
@@ -1,33 +1,47 @@
 <html>
     <head>
         <title>Porn Video HD</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />  
         <meta charset="utf-8"/>
         <link rel="stylesheet" href='<?php echo TEM_PART_NEW; ?>css/style.css'/>
+        <link rel="stylesheet" href='<?php echo TEM_PART_NEW; ?>css/animate.min.css'/>
+        <link rel="stylesheet" href='<?php echo TEM_PART_NEW; ?>css/bootstrap.min.css'/>
+        <link rel="stylesheet" href='<?php echo TEM_PART_NEW; ?>css/custom.css'/>
         <script type="text/javascript" src="<?php echo TEM_PART_NEW; ?>js/jquery-1.8.2.min.js"></script><!-- Thư viện jquery -->
+        <script type="text/javascript" src="<?php echo TEM_PART_NEW; ?>js/script.js"></script><!-- Thư viện jquery -->
         <!--<script type="text/javascript" src="ajax/update.js"></script>-->
     </head>
     <body>
-        <div id="nav">
-            <?php include('modules/layout_nav.php'); ?>
-        </div><!-- end navication -->
-        <div id="body">
-            <div id="noidung" class="noidung">
-                <?php
-                $com = 'home';
-                if (isset($_GET['com']))
-                    $com = $_GET['com'];
-                $thiscom = 'components/com_' . $com . '/layout.php';
-                if (is_file($thiscom) == true)
-                    include($thiscom);
-                else
-                    echo"Com không tồn tại";
-                ?>
-            </div><!-- end noi dung -->
-            <div class="clr"></div><!-- clear -->
-        </div><!-- end body -->
+        <header>
+            <div id="nav" style="width: 100%">
+                <?php include('modules/layout_nav.php'); ?>
+            </div><!-- end navication -->
+            <div id="slider">
+            </div><!--end slider-->
+            <div class="clear"></div>
+        </header>
+        <article>
+            <div id="main">
+                <div id="main-container" >
+                    <?php
+                    $com = 'home';
+                    if (isset($_GET['com']))
+                        $com = $_GET['com'];
+                    $thiscom = 'components/com_' . $com . '/layout.php';
+                    if (is_file($thiscom) == true)
+                        include($thiscom);
+                    else
+                        echo"Com không tồn tại";
+                    ?>
+                </div><!-- end noi dung -->
 
-        <div id="footer">Thiết kế bởi SEX_TEAM</div><!-- end footer -->
+            </div><!-- end main -->
+        </article>
+        <footer>
+            <div id="footer">Thiết kế bởi SEX_TEAM</div><!-- end footer -->
+        </footer>
 
     </body>
 </html>
